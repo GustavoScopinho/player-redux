01b46e33f51d0602465840c84dfc25bc5bf9b0c0
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2023-07-01 17:24:39 -0300

    Adicionando scroll de forma dinâmica

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,4 +1,5 @@
-import { Video } from "lucide-react";
+import * as Collapsible from '@radix-ui/react-collapsible';
+
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 
@@ -11,8 +12,8 @@ interface ModuleProps {
 export function Module({moduleIndex, title, amountOfLessons}: ModuleProps) {
   return (
     <>
-        <div>
-              <button className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
+        <Collapsible.Root className='group'>
+              <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4 ">
                 <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
                   {moduleIndex + 1}
                 </div>
@@ -21,15 +22,18 @@ export function Module({moduleIndex, title, amountOfLessons}: ModuleProps) {
                   <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
                 </div>
 
-                <ChevronDown className="w-4 h-5 ml-auto text-zinc-400"/>
-              </button>
+                <ChevronDown className="w-4 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180"/>
+              </Collapsible.Trigger>
 
+            <Collapsible.Content>
               <nav className="relative flex flex-col gap-4 p-6">
                 <Lesson title='Fundamentos do Redux' duration='09:13' />
                 <Lesson title='Fundamentos do Redux' duration='09:13' />
                 <Lesson title='Fundamentos do Redux' duration='09:13' />
               </nav>
-        </div>
+            </Collapsible.Content>
+              
+        </Collapsible.Root>
     </>
   )
 }
